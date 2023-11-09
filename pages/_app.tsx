bf0fc3f930851e5e1d3d196c9ebad44a9ed8a12f
Rafael Ferreira Umbelino <rafael.umbelino@encora.com>
2023-11-09 14:02:17 -0300

    starting nextjs project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import type { AppProps } from 'next/app';
+import { Container } from 'react-bootstrap';
+import { appWithTranslation } from 'next-i18next';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => {
+	return (
+		<Container>
+			<Component {...pageProps} />
+		</Container>
+	);
+};
+
+export default appWithTranslation(App);
